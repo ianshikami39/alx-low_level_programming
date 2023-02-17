0df2aa527af3693f5b577e0b5a59fce4ab4747fd
ianshikami39 <ianshikami39@gmail.com>
2023-02-17 02:14:55 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- * Description: This func prints a message to the console using the printf()
- *  * Return: Always 0
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-	printf("%s", "with proper gramar,but the outcome is a piece of art,\n");
+	printf("with proper gramar,but the outcome is a piece of art,\n");
 	return (0);
 }
