48d88c3888bfee73db08c25c0a79787854961ef1
ianshikami39 <ianshikami39@gmail.com>
2023-02-17 02:04:01 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- * Description: This func prints a message to the console using the print()
- *  * Return:Always 0
+ * main - Entry point
+ * Description: This func prints a message to the console using the printf()
+ *  * Return: Always 0
  */
-
 int main(void)
 {
 	printf("%s", "with proper gramar,but the outcome is a piece of art,\n");
