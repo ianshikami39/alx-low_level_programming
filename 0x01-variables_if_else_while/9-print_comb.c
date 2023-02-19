3b0afec55ef70b9fc202e7fb23b1c9485dbaea82
ianshikami39 <ianshikami39@gmail.com>
2023-02-19 13:40:39 -0800

    print comb3.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,21 @@
  *
  * Return: always 0
  */
-
+ 
 int main(void)
 {
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
-		if (i != 9)
+		if (i == 9)
+			putchar(i + '0');
+		else
 		{	
+			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
