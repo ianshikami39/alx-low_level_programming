01769b0a52cc489f1f315b1a25d1d9a19cdff03e
ianshikami39 <ianshikami39@gmail.com>
2023-02-21 05:45:43 -0800

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void);
 {
-	print_alphabet_x10();
-	return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
