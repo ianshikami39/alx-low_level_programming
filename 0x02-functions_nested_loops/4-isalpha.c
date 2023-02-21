77aa6434de42d3a1882a5755336946982c6d7252
ianshikami39 <ianshikami39@gmail.com>
2023-02-21 10:42:12 -0800

    4 isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,12 +5,11 @@
  *
  * @c: parameter to be checked
  *
- * Return: 1 if it is an alphabet
- * and 0 otherwise
+ * Return: 1 if it is an alphabet and 0 otherwise
  */
 
 int _isalpha(int c)
-{	
+{
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
