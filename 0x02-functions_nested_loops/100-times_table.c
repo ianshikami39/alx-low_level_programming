61f781d6bd22bca6a423c141f0558e57c1cabc3f
ianshikami39 <ianshikami39@gmail.com>
2023-02-22 12:42:00 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,30 +14,38 @@ void print_times_table(int n)
 	{
 	for (num = 0; num <= n; num++)
 	{
-	_putchar('0');
-	for (mult = 1; mult <= n; mult++)
+	for (mult = 0; mult <= n; mult++)
+	{
+	prod = num * mult;
+	if (prod > 99)
 	{
 	_putchar(',');
-	_putchar(' ');
-	prod = num * mult;
-	if (prod <= 99)
-	{
-	_putchar(' ');
-	if (prod <= 9)
-	{
-	_putchar(' ');
-	if (prod >= 100)
-	{
+	_putchar(32);
 	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
-	}
+	_putchar(((prod / 10) % 10) + '0');
 	_putchar((prod % 10) + '0');
 	}
-	_putchar('n');
+	else if (prod > 9)
+	{
+	_putchar(' ');
+	_putchar(32);
+	_putchar(32);
+	_putchar(((prod / 10) % 10) + '0');
+	_putchar((prod % 10) + '0');
+	}
+	else if (mult != 0)
+	{
+	_putchar(',');
+	_putchar(32);
+	_putchar(32);
+	_putchar(32);
+	}
+	else
+	{
+	_putchar(prod + '0');
+	}
+	_putchar('\n');
+	}
 	}
 	}
 }
