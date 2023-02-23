97c2265ae4554eb1c0ce40760a519807154185d1
ianshikami39 <ianshikami39@gmail.com>
2023-02-22 22:24:55 -0800

    0 task

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,16 +1,17 @@
-#include "main.h"
+#ifndef main_h
+#define main_h
 
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
+#endif
