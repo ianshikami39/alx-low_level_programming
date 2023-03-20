a350d52e5b2528f0fbeb49b492b3bfae9a1aec14
ianshikami39 <ianshikami39@gmail.com>
2023-03-20 08:39:50 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,6 +5,9 @@
  * File: 3-function_like_macro.h
  * Desc: Defines a function-like macro ABS(x)
  * that computes the absolute value of a number x
+ * The ternary operator is used by the macro to check if x is less than 0
+ * if x is less than 0, the macro returns -x 
+ * if x is greater than or equal to 0 the macro returns x.
  */
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
