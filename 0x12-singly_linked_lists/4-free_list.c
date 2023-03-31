7b34f9719442914121bf895725d43dec52c0e26c
ianshikami39 <ianshikami39@gmail.com>
2023-03-31 12:42:56 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s {
+	char *str;
+	struct list_s *next;
+} list_t;
+
+/**
+ * free_list - frees a list
+ * @head: head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	
+	while (current != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+		current = head;
+	}
+}
