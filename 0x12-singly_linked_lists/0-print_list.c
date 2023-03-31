340918c7af1cf3304dcd84b8b619b5f12ac95a34
ianshikami39 <ianshikami39@gmail.com>
2023-03-31 11:56:18 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%lu] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
